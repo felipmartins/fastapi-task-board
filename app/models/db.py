1737c007f728993c35bae3921b837cb2ae9be080
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-04 18:30:10 -0300

    refactor: db file

---
+++ app/models/db.py
@@ -0,0 +1,22 @@
+from . import board_model, task_model
+from sqlmodel import  SQLModel, create_engine, Session
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+SQLModel.metadata.create_all(engine)
+
+def test_board_insertion():
+    board = board_model.Board(title='Board do Felps')
+    with Session(engine) as session:
+        session.add(board)
+        session.commit()
+
+def test_task_insertion():
+    task = task_model.Task(title='Task do Felps', description='Description of Task do Felps')
+    with Session(engine) as session:
+        session.add(task)
+        session.commit()
