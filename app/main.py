8e49f275cfe51e14dcc20d7fbd1d40607fd6afbb
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-04 17:56:17 -0300

    refactor: index route

--- app/main.py
+++ app/main.py
@@ -5,5 +5,5 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "hello world"}
+async def home():
+    return {"info": "this is the index route of the task board API, use /board to interact with boards and /board/{id} to retrieve all tasks associated with the chosen board"}
