815564cb1884e688f308b4d5a0dffd89eca234cf
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-06 19:02:06 -0300

    implements board routes

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -1,23 +1,42 @@
 from fastapi import APIRouter
-
+from app.models.db import engine
+from app.models.board_model import Board
+from sqlmodel import Session, select
 router = APIRouter(prefix="/board", tags=["boards"])
 
 
 @router.get("/")
 async def list_boards():
-    return ...
+    with Session(engine) as session:
+        boards = select(Board)
+        query_result = session.exec(boards)
+        return list(query_result)
 
 
 @router.post("/")
-async def new_board():
-    return ...
+async def new_board(req_board: Board):
+    with Session(engine) as session:
+        new_board = Board(title=req_board.title)
+        session.add(new_board)
+        session.commit()
+        return 'created'
 
 
-@router.put("/")
-async def edit_board():
-    return ...
+@router.put("/{id}")
+async def edit_board(id: int, req_board: Board):
+    with Session(engine) as session:
+        board = session.exec(select(Board).where(Board.id == id)).one()
+        board.title = req_board.title
+        session.add(board)
+        session.commit()
+        session.refresh(board)
+        return 'edited'
 
 
-@router.delete("/")
-async def delete_board():
-    return ...
+@router.delete("/{id}")
+async def delete_board(id: int):
+    with Session(engine) as session:
+        board = session.exec(select(Board).where(Board.id == id)).one()
+        session.delete(board)
+        session.commit()
+        return 'deleted'
