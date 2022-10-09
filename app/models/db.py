e95b6f1311ac08d7e9294b20c1ddab4bba1ea0c8
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-09 08:26:07 -0300

    remove test functions once tests were complete

--- app/models/db.py
+++ app/models/db.py
@@ -8,18 +8,3 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 engine = create_engine(sqlite_url, echo=True)
 
 SQLModel.metadata.create_all(engine)
-
-def test_board_insertion():
-    board = board_model.Board(title='Board do Felps')
-    with Session(engine) as session:
-        session.add(board)
-        session.commit()
-
-def test_task_insertion():
-    task = task_model.Task(board_id = 1, title='Task do Felps', description='Description of Task do Felps')
-    with Session(engine) as session:
-        session.add(task)
-        session.commit()
-
-test_board_insertion()
-test_task_insertion()
